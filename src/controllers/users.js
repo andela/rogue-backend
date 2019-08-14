145167c6ef1698b16e0b0ba7e54c0cf7a558eb97
Christian <nwodochristian@gmail.com>
2019-08-14 16:05:31 +0100

    update develop branch

---
+++ src/controllers/users.js
@@ -0,0 +1,21 @@
+const { Sequelize, sequelize } = require('../models/index')
+const Users = require('../models/user')(sequelize, Sequelize.DataTypes);
+const timeStamp = Date.now().toString();
+const id = Math.round(parseInt(timeStamp, 10) / 1000);
+console.log(id, typeof id)
+
+module.exports = {
+    create(req, res) {
+        const { name, email, password } = req.body;
+        return Users.create({
+            name,
+            email,
+            password,
+            id,
+
+
+        })
+            .then(user => res.status(201).send(user))
+            .catch(error => res.status(400).send(error))
+    }
+}
