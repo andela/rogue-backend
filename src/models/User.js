ee0fb4620f6b9c34c8b24380eddff68051c0b602
minlinx <minaproblemsolver@gmail.com>
2019-08-14 22:03:44 +0100

    chore(eslint): configures eslint for the project

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 const crypto = require("crypto");
-const secret = require("../config").secret;
+const { secret } = require("../config");
 
 const UserSchema = new mongoose.Schema(
     {
@@ -33,21 +33,21 @@ const UserSchema = new mongoose.Schema(
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-UserSchema.methods.validPassword = function(password) {
+UserSchema.methods.validPassword = function (password) {
     const hash = crypto
         .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
         .toString("hex");
     return this.hash === hash;
 };
 
-UserSchema.methods.setPassword = function(password) {
+UserSchema.methods.setPassword = function (password) {
     this.salt = crypto.randomBytes(16).toString("hex");
     this.hash = crypto
         .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
         .toString("hex");
 };
 
-UserSchema.methods.toAuthJSON = function() {
+UserSchema.methods.toAuthJSON = function () {
     return {
         username: this.username,
         email: this.email
